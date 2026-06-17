31e9a6cf0cbaad731a8f59ca40ab986014a77e8b
Usuario invitado <Guest@iMacdePCEA10804.PUCE.EDU.EC>
2026-06-17 15:48:08 -0500

    Laboratorio 7 finalizado 17 de junio de  2026

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,5 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
@@ -7,16 +6,31 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Perfil de Usuario</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Perfil de Usuario</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+
+        <div className="card-contaider">
+          <IonCard className="card">
+            <img src="https://logodownload.org/wp-content/uploads/2020/02/barcelona-sc-logo-6.png" alt="Avatar" />
+            <IonCardHeader>
+              <IonCardTitle>Mateo Rivera Escalante</IonCardTitle>
+              <IonCardSubtitle>mateoriveraescalante</IonCardSubtitle>
+            </IonCardHeader>
+            <IonCardContent>
+                Desarrollador de software con experiencia en 
+                  aplicaciones moviles, hincha a muerte del idolo 
+                  del Ecuador, Ecuatoriano!
+            </IonCardContent>
+          </IonCard>
+        </div>
+
       </IonContent>
     </IonPage>
   );
